541e6e38f2e854538354c18cc07207b297b4ef48
Mathias Allagnat <mathiasallagnat@gmail.com>
2019-08-20 13:35:32 +0200

    api synchronized with geocoder

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,26 @@
 import "bootstrap";
+import { injectCoordinates} from '../components/geocode';
+import mapboxgl from 'mapbox-gl';
+
+
+const formElement = document.getElementById('searchForm');
+formElement.addEventListener('submit', (event) => {
+  // prevent form's default behavior
+  event.preventDefault();
+
+  // get input value
+  const address = document.getElementById('address').value;
+  const mapboxKey = 'yourApiKey';
+  // trigger request to Google Maps API
+  fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=pk.eyJ1IjoibWF0aGlhczIxODkiLCJhIjoiY2p6YjlsMTM1MDhjMTNncGg0M3M2Ymx3bCJ9.5DmaCa-Xj2popxvUOIeglQ`)
+    .then(response => response.json())
+    .then((data) => {
+      const coordinates = {
+        lng: data.features[0].geometry.coordinates[0],
+        lat: data.features[0].geometry.coordinates[1]
+      };
+      injectCoordinates(coordinates);
+      });
+});
+
+
