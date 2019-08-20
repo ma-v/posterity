bc1185a3eeab5841a244c85df12a1438c19bafb1
Michael Brunel <brunel.michael@gmail.com>
2019-08-20 12:25:38 +0200

    mapbox api added + track draw done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,10 @@
 import "bootstrap";
+
+import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
+import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css'
+
+import { initMapbox } from '../plugins/init_mapbox';
+import { AddTracking } from '../plugins/init_mapbox';
+
+var map = initMapbox();
+AddTracking(map);
