620ee92a9676f948cef98c6bf4d08f6a90518fba
Mavrick Munoz <mavrick.munoz@gmail.com>
2019-08-20 14:39:39 +0200

    last update

--- app/javascript/components/select-rides.js
+++ app/javascript/components/select-rides.js
@@ -1,16 +1,17 @@
 const polyline = require('@mapbox/polyline');
 
 const selectRide = () => {
+	let coordinates = []; 
 	const displayCoordinates = document.querySelector("#coordinates");
 	for (var i = 0; i <= 9; i++) {
   		const button_i = document.querySelector(`#activity_${i}`);
   		const polyline_i = button_i.dataset.polyline;
   		button_i.addEventListener("click", function() {
-  			const coordinates = polyline.toGeoJSON(`${polyline_i}`)
-  			displayCoordinates.innerText = coordinates["coordinates"]
-
+  			coordinates[i] = polyline.toGeoJSON(`${polyline_i}`);
+  			displayCoordinates.innerText = coordinates[i]["coordinates"];
   		})
 	}	
+	return coordinates;
 }
 
 export {selectRide};
