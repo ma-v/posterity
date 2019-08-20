706c1bbf4a160c97f50b6aded77a201c961f5cf4
Mavrick Munoz <mavrick.munoz@gmail.com>
2019-08-20 13:46:30 +0200

    decode polylines into array of coordinates

--- app/javascript/components/select-rides.js
+++ app/javascript/components/select-rides.js
@@ -1,10 +1,14 @@
+const polyline = require('@mapbox/polyline');
+
 const selectRide = () => {
-	const polyline = document.querySelector("#polyline");
+	const displayCoordinates = document.querySelector("#coordinates");
 	for (var i = 0; i <= 9; i++) {
   		const button_i = document.querySelector(`#activity_${i}`);
   		const polyline_i = button_i.dataset.polyline;
   		button_i.addEventListener("click", function() {
-  			polyline.innerText = polyline_i
+  			const coordinates = polyline.toGeoJSON(`${polyline_i}`)
+  			displayCoordinates.innerText = coordinates["coordinates"]
+
   		})
 	}	
 }
