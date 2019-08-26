b973ceffd37f8238b456f915b09f0a89895f9b58
Mavrick Munoz <mavrick.munoz@gmail.com>
2019-08-26 10:18:25 +0200

    rides print on the map switched to mapbox

--- app/javascript/actions/init-map.js
+++ app/javascript/actions/init-map.js
@@ -1,7 +1,10 @@
 import mapboxgl from 'mapbox-gl';
 import printPdf from 'mapbox-print-pdf';
+import mapboxDraw from '@mapbox/mapbox-gl-draw';
+import polyline from '@mapbox/polyline';
 
 let map = null;
+
 
  // créer la map avec Mapbox
 const initMap = () => {
@@ -11,7 +14,7 @@ const initMap = () => {
     map = new mapboxgl.Map({
       container: 'mapid',
       style: 'mapbox://styles/mapbox/streets-v10',
-      center: [-122.48369693756104, 37.83381888486939],//[5.400000, 43.300000],
+      center: [5.400000, 43.300000],//[5.400000, 43.300000],
       zoom: 15
     });
   }
@@ -32,96 +35,64 @@ const initMap = () => {
     });
 }
 
-
-
  const selectRide = () => {
   let coordinates = [];
   const displayCoordinates = document.querySelector("#coordinates");
   const imageInput = document.querySelector("#map_image");
   const titleInput = document.querySelector("#map_title");
-  // const traces = L.featureGroup();
+  // const traces = L.featureGroup(); --> BOUNDS LIMITS
   for (var i = 1; i <= 10; i++) {
     const button_i = document.querySelector(`#activity_${i}`);
     let j = i;
 
      if (button_i) {
       button_i.addEventListener("click", function () {
-        map.addLayer({
-          "id": "route",
-          "type": "line",
-          "source": {
-            "type": "geojson",
-            "data": {
-              "type": "Feature",
-              "properties": {},
-              "geometry": {
-                "type": "LineString",
-                "coordinates": [
-                  [-122.48369693756104, 37.83381888486939],
-                  [-122.48348236083984, 37.83317489144141],
-                  [-122.48339653015138, 37.83270036637107],
-                  [-122.48356819152832, 37.832056363179625],
-                  [-122.48404026031496, 37.83114119107971],
-                  [-122.48404026031496, 37.83049717427869],
-                  [-122.48348236083984, 37.829920943955045],
-                  [-122.48356819152832, 37.82954808664175],
-                  [-122.48507022857666, 37.82944639795659],
-                  [-122.48610019683838, 37.82880236636284],
-                  [-122.48695850372314, 37.82931081282506],
-                  [-122.48700141906738, 37.83080223556934],
-                  [-122.48751640319824, 37.83168351665737],
-                  [-122.48803138732912, 37.832158048267786],
-                  [-122.48888969421387, 37.83297152392784],
-                  [-122.48987674713133, 37.83263257682617],
-                  [-122.49043464660643, 37.832937629287755],
-                  [-122.49125003814696, 37.832429207817725],
-                  [-122.49163627624512, 37.832564787218985],
-                  [-122.49223709106445, 37.83337825839438],
-                  [-122.49378204345702, 37.83368330777276],
-                  [-122.48369693756104, 37.83381888486939],
-                  [-122.48348236083984, 37.83317489144141],
-                  [-122.48339653015138, 37.83270036637107],
-                  [-122.48356819152832, 37.832056363179625],
-                  [-122.48404026031496, 37.83114119107971],
-                  [-122.48404026031496, 37.83049717427869],
-                  [-122.48348236083984, 37.829920943955045],
-                  [-122.48356819152832, 37.82954808664175],
-                  [-122.48507022857666, 37.82944639795659],
-                  [-122.48610019683838, 37.82880236636284],
-                  [-122.48695850372314, 37.82931081282506],
-                  [-122.48700141906738, 37.83080223556934],
-                  [-122.48751640319824, 37.83168351665737],
-                  [-122.48803138732912, 37.832158048267786],
-                  [-122.48888969421387, 37.83297152392784],
-                  [-122.48987674713133, 37.83263257682617],
-                  [-122.49043464660643, 37.832937629287755],
-                  [-122.49125003814696, 37.832429207817725],
-                  [-122.49163627624512, 37.832564787218985],
-                  [-122.49223709106445, 37.83337825839438],
-                  [-122.49378204345702, 37.83368330777276],
-                  [-122.48369693756104, 37.83381888486939],
-                  [-122.48348236083984, 37.83317489144141],
-                  [-122.48339653015138, 37.83270036637107],
-                  [-122.48356819152832, 37.832056363179625],
-                  [-122.48404026031496, 37.83114119107971],
-                  [-122.48404026031496, 37.83049717427869],
-                  [-122.48348236083984, 37.829920943955045]
-                ]
+        event.currentTarget.classList.toggle("pressed");
+        var polyline_i = button_i.dataset.polyline;
+        coordinates[j] = polyline.toGeoJSON(`${polyline_i}`);
+        let myCoordinates = coordinates[j].coordinates;
+        // const newArray = myCoordinates.map(function(ar) {
+        //     const newAr = [];
+        //     newAr[0] = ar[1];
+        //     newAr[1] = ar[0];
+        //     return newAr;
+        //   });
+        if (event.currentTarget.classList.contains("pressed")) { 
+          map.addLayer({
+            "id": `route_${j}`,
+            "type": "line",
+            "source": {
+              "type": "geojson",
+              "data": {
+                "type": "Feature",
+                "properties": {},
+                "geometry": {
+                  "type": "LineString",
+                  "coordinates": myCoordinates
+                  }
               }
+            },
+            "layout": {
+              "line-join": "round",
+              "line-cap": "round"
+            },
+            "paint": {
+              "line-color": "#0214BB",
+              "line-width": 5
             }
-          },
-          "layout": {
-            "line-join": "round",
-            "line-cap": "round"
-          },
-          "paint": {
-            "line-color": "#888",
-            "line-width": 8
-          }
-        });
+          });
+        }
+
+        else {
+
+        }
+
+        //document.map.fitBounds(traces.getBounds());
+        titleInput.value = button_i.dataset.title;
+        imageInput.value = button_i.dataset.image;
       });
     }
   };
-}
+};
 
  export { initMap };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,7 @@ import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css';
 import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css'
 
+
 //IMPORT JS
 import { initMap } from '../actions/init-map';
 
