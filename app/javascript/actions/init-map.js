fa8e56ef57e6b825727bb6efb5fa417f041dd293
Kevin Berthier <kevinberthier@orange.fr>
2019-08-26 23:12:42 +0200

    autoZoom multiLayers

--- app/javascript/actions/init-map.js
+++ app/javascript/actions/init-map.js
@@ -5,8 +5,7 @@ import polyline from '@mapbox/polyline';
 
 let map = null;
 
-
- // créer la map avec Mapbox
+// créer la map avec Mapbox
 const initMap = () => {
   const mapElement = document.getElementById('mapid');
   if (mapElement) {
@@ -36,14 +35,14 @@ if (layerList) {
   }
 
   for (let i = 0; i < inputs.length; i++) {
-  inputs[i].onclick = switchLayer;
-  }  
+    inputs[i].onclick = switchLayer;
+  }
 }
 
 
 
 
- const printMap = () => {
+const printMap = () => {
   printPdf.build()
     .format('a3') // valeur à récuperer dans le DOM
     .portrait() // Unnecessary since it's the default but it's included for clarity.
@@ -53,88 +52,67 @@ if (layerList) {
     });
 };
 
- const selectRide = () => {
-  let coordinates = [];
-  // let allCoordinates = [];
-  const displayCoordinates = document.querySelector("#coordinates");
-  const imageInput = document.querySelector("#map_image");
-  const titleInput = document.querySelector("#map_title");
-  // const traces = L.featureGroup(); --> BOUNDS LIMITS
+const selectRide = () => {
 
-  for (var i = 1; i <= 10; i++) {
-    const button_i = document.querySelector(`#activity_${i}`);
-    let j = i;
-
-     if (button_i) {
-      button_i.addEventListener("click", function () {
-        event.currentTarget.classList.toggle("pressed");
-        var polyline_i = button_i.dataset.polyline;
-        coordinates[j] = polyline.toGeoJSON(`${polyline_i}`);
-        let myCoordinates = coordinates[j].coordinates;
-        // allCoordinates.push(myCoordinates);
-        // console.log(allCoordinates[0]);
-
-        // const newArray = myCoordinates.map(function(ar) {
-        //     const newAr = [];
-        //     newAr[0] = ar[1];
-        //     newAr[1] = ar[0];
-        //     return newAr;
-        //   });
-        if (event.currentTarget.classList.contains("pressed")) {
+  const allCoordinates = []
+  document.querySelectorAll('.activity-btn').forEach(activityBtn => {
+    // array of objects : key is id, value is an array of coordinates
+    const id = activityBtn.dataset.id
+    let polyline_i = activityBtn.dataset.polyline;
+    allCoordinates[id] = polyline.toGeoJSON(`${polyline_i}`).coordinates;
+  });
 
 
-          // if (typeof mapLayer !== 'undefined') {
-          //   map.setLayoutProperty(`route_${j}`, 'visibility', 'visible');
-          // }
+  document.querySelectorAll('.activity-btn').forEach(activityBtn => {
+    activityBtn.addEventListener("click", () => {
+      event.currentTarget.classList.toggle("pressed");
 
-            map.addLayer({
-              "id": `route_${j}`,
-              "type": "line",
-              "source": {
-                "type": "geojson",
-                "data": {
-                  "type": "Feature",
-                  "properties": {},
-                  "geometry": {
-                    "type": "LineString",
-                    "coordinates": myCoordinates
-                    }
-                }
-              },
-              "layout": {
-                "line-join": "round",
-                "line-cap": "round",
-                "visibility": "visible"
-              },
-              "paint": {
-                "line-color": "#0214BB",
-                "line-width": 5
+      const id = activityBtn.dataset.id
+      if (activityBtn.classList.contains("pressed")) {
+        map.addLayer({
+          "id": `route_${id}`,
+          "type": "line",
+          "source": {
+            "type": "geojson",
+            "data": {
+              "type": "Feature",
+              "properties": {},
+              "geometry": {
+                "type": "LineString",
+                "coordinates": allCoordinates[id]
               }
-            });
-            // map.setLayoutProperty(`route_${j}`, 'visibility', 'visible')
+            }
+          },
+          "layout": {
+            "line-join": "round",
+            "line-cap": "round",
+            "visibility": "visible"
+          },
+          "paint": {
+            "line-color": "#0214BB",
+            "line-width": 5
           }
-          else {
-            map.setLayoutProperty(`route_${j}`, 'visibility', 'none');
-            map.removeLayer(`route_${j}`);
-            map.removeSource(`route_${j}`);
-          }
-
-        // const reducer = (accumulator, currentValue) => accumulator + currentValue;
-        // allCoordinates = allCoordinates.reduce(reducer);
-        var bounds = myCoordinates.reduce(function(bounds, coord) {
-        return bounds.extend(coord);
-        }, new mapboxgl.LngLatBounds(myCoordinates[0], myCoordinates[0]));
-
-        map.fitBounds(bounds, {
-          padding: 20
         });
+      } else {
+        map.setLayoutProperty(`route_${id}`, 'visibility', 'none');
+        map.removeLayer(`route_${id}`);
+        map.removeSource(`route_${id}`);
+      }
+      let selectedCoordinates = []
+      document.querySelectorAll('.activity-btn.pressed').forEach(btn => {
+        let id = btn.dataset.id
+        allCoordinates[id].forEach(c => selectedCoordinates.push(c))
+      })
 
-        //document.map.fitBounds(traces.getBounds());
-        titleInput.value = button_i.dataset.title;
-        imageInput.value = button_i.dataset.image;
-        }
-      );
-    }
-  }
- };
- export { initMap };
+      console.log('SELECTED', selectedCoordinates);
+
+      let bounds = selectedCoordinates.reduce((bounds, coord) => bounds.extend(coord),
+        new mapboxgl.LngLatBounds(selectedCoordinates[0], selectedCoordinates[0]));
+
+      if (bounds !== []) { map.fitBounds(bounds, { padding: 20 }); }
+    });
+
+  });
+};
+
+export { initMap };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,13 @@ import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css'
 //IMPORT JS
 import { initMap } from '../actions/init-map';
 
-import {switchToCheckout} from '../actions/customize-map-box';
+
+import { switchToCheckout } from '../actions/customize-map-box';
 switchToCheckout();
 
 //INIT MAP
 initMap(); // init la map et toutes ces fonctions
+
+
+
+
