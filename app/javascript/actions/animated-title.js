82e6d11107e80419df935d7080abfaef1f304b0a
Mathias Allagnat <mathiasallagnat@gmail.com>
2019-08-27 16:37:09 +0200

    first front of the order page plus added the type js animation on the home

---
+++ app/javascript/actions/animated-title.js
@@ -0,0 +1,11 @@
+import Typed from 'typed.js';
+
+const animateTitle = () => {
+ var typed = new Typed('#h-title', {
+  strings: ["Leave a trace. Print your Records", "Leave a trace. Print your Rides","Leave a trace. Print your Marathon","Leave a trace. Print your Hike","Leave a trace. Print your Trail", "Leave a trace. Print your Iron-Man"],
+  smartBackspace: true,
+  typeSpeed: 75,
+});
+};
+
+export {animateTitle};
