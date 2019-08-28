d53f0c1e37a8f2004a40f6eae8fae004df92c940
Mathias Allagnat <mathiasallagnat@gmail.com>
2019-08-28 10:47:35 +0200

    fixed typed js

--- app/javascript/actions/animated-title.js
+++ app/javascript/actions/animated-title.js
@@ -5,6 +5,7 @@ const animateTitle = () => {
   strings: ["Leave a trace. Print your Records", "Leave a trace. Print your Rides","Leave a trace. Print your Marathon","Leave a trace. Print your Hike","Leave a trace. Print your Trail", "Leave a trace. Print your Iron-Man"],
   smartBackspace: true,
   typeSpeed: 75,
+  showCursor: false
 });
 };
 
