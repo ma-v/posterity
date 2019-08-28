af02dc70fff66b399dfdbc1d63d63f7fae7fad9c
Michael Brunel <brunel.michael@gmail.com>
2019-08-28 15:22:47 +0200

    Add some style to the track records button

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,4 +12,16 @@ import {switchToCheckout} from '../actions/customize-map-box';
 switchToCheckout();
 
 //INIT MAP
-initMap(); // init la map et toutes ces fonctions
+initMap();
+
+$(document).ready(function(){
+  $(".content").slice(0, 4).show();
+  $("#loadMore").on("click", function(e){
+    e.preventDefault();
+    $(".content:hidden").slice(0, 4).slideDown();
+    if($(".content:hidden").length == 0) {
+      $("#loadMore").text("No Content").addClass("noContent");
+    }
+  });
+
+}) // init la map et toutes ces fonctions
