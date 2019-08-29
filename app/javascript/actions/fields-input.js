e01d60569ede0301dea64f1cb9cf5f28bd1417ec
Mathias Allagnat <mathiasallagnat@gmail.com>
2019-08-29 13:58:36 +0200

    success adding data distance

--- app/javascript/actions/fields-input.js
+++ app/javascript/actions/fields-input.js
@@ -1,9 +1,27 @@
-const addfields = () => {
+import {selectRide} from './init-map';
+import {initMap} from './init-map';
+
+const addFields = () => {
   let titleFrame = document.querySelector('.map-title');
-  let speedField = document.querySelector('.ride-title');
-  if (titleField) {
-    titleField.addEventListener('keyup', (event) => {
-      titleFrame.insertAdjactentHTML = ('beforeend',`<p class="legend-title">${titleField.value}</p>`);
-    });
-  }
+  let distanceField = document.querySelector('#activity-distance');
+  distanceField.addEventListener('change', checkDistance);
 };
+
+const checkDistance = () => {
+
+  let titleFrame = document.querySelector('.map-title');
+  let distanceField = document.querySelector('#activity-distance');
+  let isChecked = distanceField.checked;
+
+     if (isChecked) {
+      titleFrame.insertAdjacentHTML("beforeend",`<p class="legend-infos">${document.dist/1000} kms</p>`);
+     } else{
+        let distanceInfo = document.querySelector('.legend-infos');
+        distanceInfo.remove();
+
+     }
+  };
+
+
+export {addFields};
+export {checkDistance};
