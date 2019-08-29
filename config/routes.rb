6e55bcd80b1543789747860ba579ed47c7ba7e7a
Mavrick Munoz <mavrick.munoz@gmail.com>
2019-08-29 14:43:13 +0200

    Stripe implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   resources :maps, only: [:new, :create] do
+  	get '/orders/confirmation', to: "orders#show"
     resources :orders, only: [:new, :create] do 
     	resources :payments, only: [:new, :create]
     end 	
   end
-  get '/orders/confirmation', to: "orders#show"
 
 end
