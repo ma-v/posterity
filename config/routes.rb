71ab30ead3ac1d2ba867bbfb5af70f165c4a030c
Mavrick Munoz <mavrick.munoz@gmail.com>
2019-09-18 11:22:09 +0200

    create admin dashboard page only accessible by admins + create my admin account

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :admins
   root to: 'pages#home'
+  get '/admin_dashboard', to: 'pages#admin_dashboard'
   resources :maps, only: [:new, :create] do
   	get '/orders/confirmation', to: "orders#show"
     resources :orders, only: [:new, :create] do 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_18_082918) do
+ActiveRecord::Schema.define(version: 2019_09_18_091513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_09_18_082918) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "locked_at"
   end
 
   create_table "maps", force: :cascade do |t|
