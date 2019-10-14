f230a8bf026df602ae30baf8fc31d1ae2f4efd20
Mavrick Munoz <mavrick.munoz@gmail.com>
2019-10-14 11:35:27 +0200

    resized so we can display all activities

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,16 +17,14 @@ switchToCheckout();
 animateTitle();
 loadLoader();
 
-//INIT MAP
-
 
 $(document).ready(function(){
-  $(".content").slice(0, 4).show();
+  $(".content").slice(0, 8).show();
   $("#loadMore").on("click", function(e){
     e.preventDefault();
-    $(".content:hidden").slice(0, 4).slideDown();
+    $(".content:hidden").slice(0, 8).slideDown();
     if($(".content:hidden").length == 0) {
-      $("#loadMore").text("No Content").addClass("noContent");
+      $("#loadMore").text("").addClass("noContent");
     }
   });
-}) // init la map et toutes ces fonctions
+});
