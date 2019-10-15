5c20c91ced9a5a828a3fd7a31a47205f33d8031d
Mavrick Munoz <mavrick.munoz@gmail.com>
2019-10-15 18:20:30 +0200

    display all traces with scrolldown

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,10 +19,10 @@ loadLoader();
 
 
 $(document).ready(function(){
-  $(".content").slice(0, 8).show();
+  $(".content").slice(0, 16).show();
   $("#loadMore").on("click", function(e){
     e.preventDefault();
-    $(".content:hidden").slice(0, 8).slideDown();
+    $(".content:hidden").slice(0, 16).slideDown();
     if($(".content:hidden").length == 0) {
       $("#loadMore").text("").addClass("noContent");
     }
