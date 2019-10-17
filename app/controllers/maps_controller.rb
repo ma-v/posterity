cef52b34488c5c70672a587c3f409442b5313be9
Mavrick Munoz <mavrick.munoz@gmail.com>
2019-10-17 12:33:39 +0200

    WIP user created with strava id when strava oath but still miss the link to orders when order and the assignement to existing user when strava id taken

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -5,15 +5,16 @@ class MapsController < ApplicationController
 		  	@response = JSON.parse(RestClient.post("https://www.strava.com/oauth/token?client_id=38164&client_secret=0b227c9387f9b5a8ce9b9833387192004098d95c&code=#{@token}&grant_type=authorization_code", {}))
 		  	@access_token = @response["access_token"]
 		  	@athlete_id = @response["athlete"]["id"]
-
-		  	@stats = JSON.parse(RestClient.get("https://www.strava.com/api/v3/athletes/#{@athlete_id}/stats?page=&per_page=", {Authorization: "Bearer #{@access_token}"}))
-		  	@biggest_ride = @stats["biggest_ride_distance"]
+		  	# @stats = JSON.parse(RestClient.get("https://www.strava.com/api/v3/athletes/#{@athlete_id}/stats?page=&per_page=", {Authorization: "Bearer #{@access_token}"}))
+		  	# @biggest_ride = @stats["biggest_ride_distance"]
 
 		  	@activities = JSON.parse(RestClient.get("https://www.strava.com/api/v3/athlete/activities?per_page=100", {Authorization: "Bearer #{@access_token}"}))
 
-
+				@user = User.new(strava_id: "#{@athlete_id}")
+				@user.save!
 		  	@map = Map.new
 		  	@map.orders << Order.new
+				@map.orders.last.user = @user
 	    else
 	      @map =Map.new
 	    end
@@ -42,6 +43,6 @@ class MapsController < ApplicationController
 	private
 
 	def map_params
-		params.permit(:title, :image, :map_url, :format, :distance, :time, :elevation, :speed, orders_attributes:[:first_name, :last_name, :address, :map_sku, :state, :email])
+		params.permit(:title, :image, :map_url, :format, :distance, :time, :elevation, :speed, orders_attributes:[:first_name, :last_name, :address, :map_sku, :state, :email, :user_id])
 	end
 end
