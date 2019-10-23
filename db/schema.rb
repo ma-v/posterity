9e03cd3a23d85f8dabb5527a0d867bf8fd788724
Mavrick Munoz <mavrick.munoz@gmail.com>
2019-10-23 10:00:35 +0200

    phone added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_18_163552) do
+ActiveRecord::Schema.define(version: 2019_10_23_075635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2019_10_18_163552) do
     t.string "post_code"
     t.string "city"
     t.string "country"
+    t.string "phone"
     t.index ["map_id"], name: "index_orders_on_map_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
